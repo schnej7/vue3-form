01b68e8e5c07bcd91e489a68b183d3dc2dcf3a3c
Jerry Schneider <schnej7@gmail.com>
2024-04-19 13:44:38 -0400

    Fix validator bug

--- form-example/src/constants/FormData.ts
+++ form-example/src/constants/FormData.ts
@@ -41,7 +41,7 @@ class FormData {
         const errors = [];
         if (formData.donation !== '' && formData.donation < 0) {
           errors.push('Donation cannot be negative');
-        } else if (formData.donation !== null && (formData.donation as number) * 100 % 1 !== 0) {
+        } else if (formData.donation !== null && !/^\s*-?\d+(\.\d{1,2})?\s*$/.test(formData.donation)) {
           errors.push('Donation cannot contain fractional cents');
         }
         return errors;
